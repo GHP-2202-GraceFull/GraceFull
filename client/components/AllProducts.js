ee24e0d682ef1f3c0e07a7ce125b55323e299370
laurynnlowe <laurynnlowe@gmail.com>
2022-04-19 14:55:16 -0400

    draft AllProducts React

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,11 +1,47 @@
-import React, {useState, useEffect } from "react";
+import React, {useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllProducts } from "../store/allProducts" 
 
 
-export default function AllProducts (props){
-    const [products, setProduct] = useState([]);
-    const []
+const AllProducts = () => {
+    const dispatch = useDispatch()
+    const products = useSelector((state) => {
+        return state.products
+    })
+
+    useEffect(()=> {
+        dispatch(fetchAllProducts())
+    }, [])
 
     return (
-
+        <div>
+            <h1>All Products</h1>
+            <div>
+                {products.map((product) =>{
+                    <div key={product.id}>
+                        <image scr={product.image} />
+                        <h3>{product.name}</h3>
+                        <h3>{product.price}</h3>
+                    </div>
+                })}
+            </div>
+        </div>
     )
 }
+
+
+export default AllProducts;
+
+// const mapState = (state) => {
+//     return {
+//         products: state.products
+//     }
+// }
+
+// const mapDispatch = (dispatch) => {
+//     return {
+//         loadProduct: () =>dispatch(fetchProducts())
+//     }
+// }
+
+// export default connect (null, mapDispatch)(AllProducts)
