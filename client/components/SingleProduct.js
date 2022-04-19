2f912be47b0d92269316d4d36e84c5beb43dcbb9
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-19 16:05:20 -0400

    small change to single product view before merging main

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector, useSelector } from "react-redux";
+import getSingleProduct from "../store/singleProduct";
 
+const dummySingleProduct = {};
 const SingleProduct = (productId) => {
   const dispatch = useDispatch();
   const singleProduct = useSelector((state) => state.singleProduct);
