417565cde3462e684a45e747ad3969a9666b5165
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-19 16:26:06 -0400

    small changes before merging main

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector, useSelector } from "react-redux";
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import getSingleProduct from "../store/singleProduct";
 
-const dummySingleProduct = {};
 const SingleProduct = (productId) => {
   const dispatch = useDispatch();
   const singleProduct = useSelector((state) => state.singleProduct);
@@ -12,7 +12,7 @@ const SingleProduct = (productId) => {
   return (
     <div>
       <h1>{singleProduct.title}</h1>
-      <img src={singleProduct.imageUrl} />
+      <img src={singleProduct.imageUrl} className="single-product-image" />
       {singleProduct.quantity === 0 ? (
         <h4 className="sold-out">Sold Out!</h4>
       ) : (
