9bb5d74a1adb5c3606bdb1bbccca64956d69de52
laurynnlowe <laurynnlowe@gmail.com>
2022-04-19 12:38:45 -0400

    test push

---
+++ client/components/AllProducts.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect } from "react";
+
+
+export default function AllProducts (props){
+    const [products, setProduct] = useState([]);
+    const []
+
+    return (
+
+    )
+}
