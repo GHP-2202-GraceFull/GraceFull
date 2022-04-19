0a3d0e376d6cdaf49e58667dfd3f5a4087566ee0
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-19 13:20:00 -0400

    create products model and products dummy data

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,15 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
 
 //associations could go here!
+//Product - Category = Many to Many
 
 module.exports = {
   db,
   models: {
     User,
   },
-}
+};
