f0d7aedcb241961e8a68a2a27525255afc4c2106
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-19 16:02:44 -0400

    add functional component skeleton for single product view

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector, useSelector } from "react-redux";
 
-const SingleProduct = () => {
-  return <div></div>;
+const SingleProduct = (productId) => {
+  const dispatch = useDispatch();
+  const singleProduct = useSelector((state) => state.singleProduct);
+  useEffect(() => {
+    dispatch(getSingleProduct(productId));
+  });
+  return (
+    <div>
+      <h1>{singleProduct.title}</h1>
+      <img src={singleProduct.imageUrl} />
+      {singleProduct.quantity === 0 ? (
+        <h4 className="sold-out">Sold Out!</h4>
+      ) : (
+        <></>
+      )}
+      <p>{singleProduct.description}</p>
+    </div>
+  );
 };
 
 export default SingleProduct;
