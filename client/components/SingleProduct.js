6c219c3ab79ee33578bfdb2295f2e36080aa425f
laurynnlowe <laurynnlowe@gmail.com>
2022-04-20 11:36:31 -0400

    ready to merge

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleProduct } from "../store/singleProduct";
+import AddToCart from "./AddToCart";
 
 const SingleProduct = (props) => {
   const studentId = props.match.params.id;
@@ -15,6 +16,7 @@ const SingleProduct = (props) => {
   return (
     <div>
       <h1>{singleProduct.title}</h1>
+      <AddToCart productId = {singleProduct.id} />
       <img src={singleProduct.imageUrl} className="single-product-image" />
       {singleProduct.quantity === 0 ? (
         <h4 className="sold-out">Sold Out!</h4>
