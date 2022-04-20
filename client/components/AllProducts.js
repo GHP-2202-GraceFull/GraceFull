bdce9d4eedb7d7e2c15b4c22c9b82249b13514f1
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-20 10:44:06 -0400

    set up useState for filter

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,37 +1,39 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAllProducts } from "../store/allProducts" 
-
+import { fetchAllProducts } from "../store/allProducts";
 
 const AllProducts = () => {
-    const [sort, setSort] = useState(null);
-    const dispatch = useDispatch();
-    const products = useSelector((state) => {
-        return state.allProducts
-    })
+  const [sort, setSort] = useState(null);
+  const [filter, setFilter] = useState(null);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => {
+    return state.allProducts;
+  });
 
-    useEffect(()=> {
-        dispatch(fetchAllProducts())
-    }, [])
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, []);
 
-    sort==='lowHigh'  ? products.sort((a,b)=> a.price-b.price) 
-        : sort==='highLow' ? products.sort((a,b)=> b.price-a.price) 
-        : null;
+  sort === "lowHigh"
+    ? products.sort((a, b) => a.price - b.price)
+    : sort === "highLow"
+    ? products.sort((a, b) => b.price - a.price)
+    : null;
 
-    return (
-        <div>
-            <button onClick={()=> setSort("lowHigh")}>Price: low to high</button>
-            <button onClick={()=> setSort("highLow")}>Price: high to low</button>
-            {products.map((product)=>
-                <div key={product.id}>
-                <h3>{product.title}</h3>
-                <h3>${product.price}</h3>
-                <img src={product.imageUrl} />
-                </div>
-            )}
+  return (
+    <div>
+      <button onClick={() => setSort("lowHigh")}>Price: low to high</button>
+      <button onClick={() => setSort("highLow")}>Price: high to low</button>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.title}</h3>
+          <h3>${product.price}</h3>
+          <img src={product.imageUrl} />
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default AllProducts;
