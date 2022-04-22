98ccfaafc8b45f281bc53afbab171ba0421e4d99
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-22 17:51:35 -0400

    add more styling to Navbar- make the logged in / logged out ternary wrap a smaller section of the header

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { logout } from "../store";
 
 import { FiShoppingCart } from "react-icons/fi";
+import { MdOutlineExpandMore } from "react-icons/md";
 
 //TODO: connect
 const Navbar = ({ handleClick, isLoggedIn }) => (
@@ -11,45 +12,47 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
     {/* <h1>GraceFull</h1> */}
     <nav>
       <div id="nav-logo">
-        <Link to="/home">
+        <Link to="/">
           ✻ Grace<i>Full</i>
         </Link>
       </div>
-      {isLoggedIn ? (
-        <div className="nav-links">
-          {/* The navbar will show these links after you log in */}
-          <div>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-          </div>
-          <div>
-            <Link to="/products">Products</Link>
-          </div>
-          <div>
-            <Link to="/cart">Cart</Link>
-          </div>
+      <div className="nav-links">
+        {/* The navbar will show these links before you log in */}
+        {/* <div>
+          <Link to="/signup">Sign Up</Link>
         </div>
-      ) : (
-        <div className="nav-links">
-          {/* The navbar will show these links before you log in */}
+        <div>
+          <Link to="/login">Sign In</Link>
+        </div> */}
+        <div>
+          <Link to="/products">Products</Link>
+        </div>
+        {/* <div>
+          <Link to="/checkout">Checkout</Link>
+        </div> */}
+        {isLoggedIn ? (
+          <>
+            <div>
+              Welcome, <strong>User!</strong> <MdOutlineExpandMore />
+            </div>
+            <div>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          </>
+        ) : (
           <div>
             <Link to="/login">Sign In</Link>
           </div>
-          <div>
-            <Link to="/products">Products</Link>
-          </div>
-          <div>
-            <Link to="/checkout">Checkout</Link>
-          </div>
-          <div>
-            <span>0</span>
-            <Link to="/cart">
-              <FiShoppingCart />
-            </Link>
-          </div>
+        )}
+        <div>
+          <span>0</span>
+          <Link to="/cart">
+            <FiShoppingCart size={24} />
+          </Link>
         </div>
-      )}
+      </div>
     </nav>
   </div>
 );
