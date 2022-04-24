041cea5622886718e4e30f15e0770ec94f4d56dd
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-23 20:52:43 -0400

    create component for dropdown view for user account links (nav bar)

--- client/components/AccountDropdown.js
+++ client/components/AccountDropdown.js
@@ -1,1 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const AccountDropdown = () => {
+  return (
+    <div id="dropdown-container">
+      <div>
+        <Link>My Orders</Link>
+      </div>
+      <div>
+        <a href="#" onClick={handleClick}>
+          Sign Out
+        </a>
+      </div>
+    </div>
+  );
+};
