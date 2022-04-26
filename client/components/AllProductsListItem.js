2bc8e8edfae2d301972f401568dcb3fc36fe6138
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-04-26 18:07:23 -0400

    add more styling to all products - not full functioning

--- client/components/AllProductsListItem.js
+++ client/components/AllProductsListItem.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AddToCart from "./AddToCart";
 
 const AllProductsListItem = ({ product }) => {
+  const [hover, toggleHover] = useState(false);
   return (
-    <div id="all-products-item">
-      <h3>{product.title}</h3>
-      <h3>${product.price}</h3>
-      <AddToCart product={product} />
-      <Link to={`/products/${product.id}`}>More Info</Link>
+    <div
+      id="all-products-item"
+      onMouseOver={() => toggleHover(true)}
+      onMouseOut={() => toggleHover(false)}
+    >
+      <div id="product-info" className={hover ? "hover" : ""}>
+        <div
+          className={hover ? "short-info info-hover" : "short-info"}
+          id="product-title"
+        >
+          <h3>{product.title}</h3>
+        </div>
+        <div
+          className={hover ? "short-info info-hover" : "short-info"}
+          id="product-price"
+        >
+          <h3>${product.price}</h3>
+        </div>
+        {hover && (
+          <>
+            <AddToCart product={product} view="all-products" />
+            <Link to={`/products/${product.id}`}>More Info {">"}</Link>
+          </>
+        )}
+      </div>
       <img src={product.imageUrl} />
     </div>
   );
